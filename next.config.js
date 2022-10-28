07d1e43f0f5fa35017a3478acedf956e2c5c2af8
Joshua Caleb Bolito <88492918+JCBolito@users.noreply.github.com>
2022-10-28 18:43:20 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
-	swcMinify: true,
-	future: { webpack5: true }
+	swcMinify: true
 };
 
 module.exports = nextConfig;
